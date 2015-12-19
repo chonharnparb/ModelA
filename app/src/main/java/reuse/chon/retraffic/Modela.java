1ef503e2376e5e1a16ba9d2f4c3407e949a4ce4c
chon <chonharnparb@hotmail.com>
2015-12-19 14:46:53 +0700

    sound finish

--- app/src/main/java/reuse/chon/retraffic/Modela.java
+++ app/src/main/java/reuse/chon/retraffic/Modela.java
@@ -1,13 +1,51 @@
 package reuse.chon.retraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class Modela extends AppCompatActivity {
+    //Explicit
+    private ListView trafficListView;
+    private Button abouttimeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modela);
+
+        //Bind Widget
+        bindWidget();
+
+
+        //Button Controller
+        buttoncontroller();
+
+
+    } //Main Method
+
+    private void buttoncontroller() {
+    abouttimeButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+      // Sound Effect
+            MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_long);
+            buttonMediaPlayer.start();
+
+        } // event
+    });
     }
-}
+
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        abouttimeButton = (Button) findViewById(R.id.button);
+    }
+
+
+} // Main Class
